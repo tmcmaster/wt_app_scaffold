1551ace684ecbf6d7c552761e8068d201a00378c
Tim McMaster <tim@mcmaster.id.au>
2023-07-25 23:15:30 +1000

    updated the the examples main.dart to use the new app_platform package.

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -1,26 +1,27 @@
-import 'package:wt_app_scaffold/app_scaffolds.dart';
-import 'package:wt_app_scaffold_examples/apps/app_one.dart';
+import 'package:wt_app_scaffold/app_platform.dart';
+import 'package:wt_app_scaffold_examples/apps/app_two.dart';
 import 'package:wt_app_scaffold_examples/firebase_options.dart';
-import 'package:wt_firepod/wt_firepod.dart';
 import 'package:wt_logging/wt_logging.dart';
 
 void main() async {
   runMyApp(
     withFirebase(
-      appName: 'wt-app-scaffold',
-      firebaseOptions: DefaultFirebaseOptions.currentPlatform,
-      andAppScaffold(
-        appDetails: AppOne.details,
-        appDefinition: AppOne.definition,
+      withLogin(
+        andAppScaffold(
+          appDetails: AppTwo.details,
+          appDefinition: AppTwo.definition,
+        ),
         loginSupport: const LoginSupport(
           emailEnabled: true,
           googleEnabled: true,
         ),
-        setApplicationLogLevel: Level.warning,
       ),
+      appName: 'wt-app-scaffold',
+      firebaseOptions: DefaultFirebaseOptions.currentPlatform,
     ),
     includeObservers: [],
     includeOverrides: [],
+    setApplicationLogLevel: Level.warning,
     enableProviderMonitoring: false,
   );
 }
