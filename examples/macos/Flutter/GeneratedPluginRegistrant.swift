1a3352d41242f7ec483488fda71e1c8dd4d815df
Tim McMaster <tim@mcmaster.id.au>
2024-09-07 10:43:45 +1000

    updating library versions

--- examples/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -20,6 +20,7 @@ import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
 import url_launcher_macos
+import webview_flutter_wkwebview
 import webview_universal
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -38,5 +39,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
   WebviewUniversalPlugin.register(with: registry.registrar(forPlugin: "WebviewUniversalPlugin"))
 }
